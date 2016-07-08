63494f842d1cae83275de3033ce11b7b528266b9
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2016-07-07 23:55:37 -0500

    [ADD] consider-merging-classes-inherited (#39)
    If you have 2 or more files in a odoo module with duplicated inherit:
    ```python
    # module1/file1.py
    ...
     class TestModel(models.Model):
         _inherit = 'res.company'`
    ...
    
    # module1/file2.py
    ...
     class TestModel2(models.Model):
         _inherit = 'res.company'`
    ...
    ```
    
    Then the message `module1/__init__.py:1: [R7980(consider-merging-classes-inherited), ] Consider merging classes inherited to res.company.`

--- pylint_odoo/test_repo/broken_module/models/__init__.py
+++ pylint_odoo/test_repo/broken_module/models/__init__.py
@@ -1,3 +1,5 @@
 # coding: utf-8
 
 from . import broken_model
+from . import model_inhe1
+from . import model_inhe2
---
+++ pylint_odoo/test_repo/broken_module/models/model_inhe1.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+from openerp import models
+
+
+class TestModel(models.Model):
+    _inherit = 'res.company'
