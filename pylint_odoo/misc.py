ab34a2f9b817022b81a7358dfdfe7991dbfb3594
Moisés López <moylop260@vauxoo.com>
2016-08-01 15:05:42 -0500

    [REF] misc.py: Define initial class variables

--- pylint_odoo/misc.py
+++ pylint_odoo/misc.py
@@ -57,6 +57,10 @@ class WrapperModuleChecker(BaseChecker):
     msg_args = None
     msg_code = None
     msg_name_key = None
+    odoo_node = None
+    odoo_module_name = None
+    manifest_file = None
+    module = None
 
     def get_manifest_file(self, node_file):
         """Get manifest file path
