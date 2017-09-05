73514c62bc4a4d79c06469391ffd63f57c3f02e1
Naglis Jonaitis <naglis@users.noreply.github.com>
2017-09-05 13:13:00 +0300

    [ADD] xml-deprecated-tree-attribute: Implements check for deprecated tree attributes (#149)
    * [IMP] xml-deprecated-tree-attribute: Prepares test cases
    
    * [ADD] xml-deprecated-tree-attribute: Implements check for deprecated `colors`, `fonts` and `string` tree attributes
    
    Closes #146

--- pylint_odoo/test/main.py
+++ pylint_odoo/test/main.py
@@ -62,6 +62,7 @@ EXPECTED_ERRORS = {
     'attribute-string-redundant': 33,
     'renamed-field-parameter': 2,
     'deprecated-data-xml-node': 5,
+    'xml-deprecated-tree-attribute': 3,
     'resource-not-exist': 3,
     'website-manifest-key-not-valid-uri': 1
 }
