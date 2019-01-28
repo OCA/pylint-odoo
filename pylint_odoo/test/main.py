0a998b5925abc59084b0abc5a717ef9e4fd488a8
Moisés López <moylop260@vauxoo.com>
2019-01-28 15:03:58 -0600

    [REF] requirements: Using pylint==1.8.4 for python<=2.7 and pylint==2.2.2 for python>2.7
    [REF] tox: Using pylint2 stable release
    [REF] test: Disable profiling since it is not used
    [REF] test: Disable testing python-2.7 tests for pylint-2.2.2 since that
    they are not compatibles
    
    Signed-off-by: Moisés López <moylop260@vauxoo.com>

--- pylint_odoo/test/main.py
+++ pylint_odoo/test/main.py
@@ -6,8 +6,6 @@ from tempfile import gettempdir
 import six
 
 import unittest
-from contextlib import contextmanager
-from cProfile import Profile
 
 from pylint.lint import Run
 
@@ -74,13 +72,6 @@ EXPECTED_ERRORS = {
 }
 
 
-@contextmanager
-def profiling(profile):
-    profile.enable()
-    yield
-    profile.disable()
-
-
 class MainTest(unittest.TestCase):
     def setUp(self):
         self.default_options = [
@@ -99,19 +90,12 @@ class MainTest(unittest.TestCase):
             '--disable=all',
             '--enable=odoolint,pointless-statement,trailing-newlines',
         ]
-        self.profile = Profile()
         self.sys_path_origin = list(sys.path)
         self.maxDiff = None
         self.expected_errors = EXPECTED_ERRORS.copy()
 
     def tearDown(self):
         sys.path = list(self.sys_path_origin)
-        test = self._testMethodName
-        prefix = os.path.expanduser(os.environ.get('PYLINT_ODOO_STATS',
-                                    '~/pylint_odoo_cprofile'))
-        fstats = prefix + '_' + test + '.stats'
-        if test != 'test_10_path_dont_exist':
-            self.profile.dump_stats(fstats)
 
     def run_pylint(self, paths, extra_params=None):
         for path in paths:
@@ -121,8 +105,7 @@ class MainTest(unittest.TestCase):
             extra_params = self.default_extra_params
         sys.path.extend(paths)
         cmd = self.default_options + extra_params + paths
-        with profiling(self.profile):
-            res = Run(cmd, exit=False)
+        res = Run(cmd, exit=False)
         return res
 
     def test_10_path_dont_exist(self):
