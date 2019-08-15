16ef6b6a71f9109f616a40429868a465f377078b
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2019-08-15 12:45:58 -0500

    Revert "[ADD] method-default prefix" (#254)
    This reverts commit 586c6819539a57aae847748e47518ffd757ecc7d.
    From pull request https://github.com/OCA/pylint-odoo/pull/250
    
    In order to fix https://github.com/OCA/pylint-odoo/issues/253
    and work better this new check.

--- pylint_odoo/test/main.py
+++ pylint_odoo/test/main.py
@@ -42,7 +42,6 @@ EXPECTED_ERRORS = {
     'method-inverse': 1,
     'method-required-super': 8,
     'method-search': 1,
-    'method-default': 1,
     'missing-import-error': 7,
     'missing-manifest-dependency': 5,
     'missing-newline-extrafiles': 4,
