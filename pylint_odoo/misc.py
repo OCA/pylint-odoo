9ef4ca0b6fb305b05183d54c0dd0d0e3f8cba616
Fernanda Hernández <fernanda@vauxoo.com>
2021-01-07 23:13:40 +0000

    [ADD] add compatibility to v14.0

--- pylint_odoo/misc.py
+++ pylint_odoo/misc.py
@@ -24,7 +24,7 @@ except ImportError:
 
 DFTL_VALID_ODOO_VERSIONS = [
     '4.2', '5.0', '6.0', '6.1', '7.0', '8.0', '9.0', '10.0', '11.0', '12.0',
-    '13.0',
+    '13.0', '14.0',
 ]
 DFTL_MANIFEST_VERSION_FORMAT = r"({valid_odoo_versions})\.\d+\.\d+\.\d+$"
 
