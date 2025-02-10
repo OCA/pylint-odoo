0f4f4875b23db93aa82712feaf4d5b1b868f73e5
Pedro Rodrigues <pedro-rodri@outlook.com>
2025-02-10 14:34:00 +0000

    [FIX] prohibited-method-override: Check if caller has a func attribute

--- testing/resources/test_repo/broken_module/tests/test_model.py
+++ testing/resources/test_repo/broken_module/tests/test_model.py
@@ -28,8 +28,10 @@ class TestModel(TransactionCase):
         return super(TestModel, self).test_base_method_3()
 
     def test_base_method_3(self):
+        some_obj = TransactionCase()
         # No override applied, should not fail
         super(TestModel, self).test_base_method_1()
         super(TestModel, self).test_base_method_2()
+        some_obj.test_base_method_3()
         # Override prohibited, should fail
         return super(TestModel, self).test_base_method_3()
