2e6f50399c8f1b90c94a55428541d6a23324e1d4
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2026-07-03 16:47:19 -0600

    Revert "[FIX] translation-injection: avoid AttributeError on non-attribute calls (#573)"
    This reverts commit a960eaf6a8b8b185eafd581c0b3bdb389f1ea3e7.

--- src/pylint_odoo/checkers/odoo_addons.py
+++ src/pylint_odoo/checkers/odoo_addons.py
@@ -1218,8 +1218,7 @@ class OdooAddons(OdooBaseChecker, BaseChecker):
                     )
 
         if (
-            isinstance(node.func, nodes.Attribute)
-            and self.get_func_name(node.func) == "format"
+            self.get_func_name(node.func) == "format"
             and isinstance(node.func.expr, nodes.Call)
             and self.get_func_name(node.func.expr.func) in misc.TRANSLATION_METHODS
         ):
