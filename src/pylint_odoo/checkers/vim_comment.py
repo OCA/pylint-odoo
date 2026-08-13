890f99e3312973e3f6abf308dfb1a5b032dabcfc
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2026-08-12 18:10:02 -0600

    [IMP] vim_comment: skip all processing when the check is disabled
    Compute once per run in open() whether use-vim-comment is disabled
    (config/CLI) or does not apply to the configured valid_odoo_versions,
    and return early from process_tokens before iterating any token.
    
    The early return is re-checked per module so module-level pragmas
    disabling use-vim-comment skip the token loop as well.

--- src/pylint_odoo/checkers/vim_comment.py
+++ src/pylint_odoo/checkers/vim_comment.py
@@ -14,10 +14,22 @@ class VimComment(OdooBaseChecker, BaseTokenChecker):
     name = "odoolint"
     msgs = ODOO_MSGS
 
+    def open(self):
+        """Compute once per run if the only message of this class is disabled
+        globally (config/CLI) or not applicable to the valid_odoo_versions configured
+        """
+        super().open()
+        self._is_check_enabled = self.linter.is_message_enabled("use-vim-comment") and self.is_odoo_message_enabled(
+            "W8202"
+        )
+
     def is_vim_comment(self, comment):
         return comment.strip("# ").lower().startswith("vim:")
 
     def process_tokens(self, tokens):
+        # Re-checked per module so module-level "disable=use-vim-comment" pragmas skip the loop too
+        if not self._is_check_enabled or not self.linter.is_message_enabled("use-vim-comment"):
+            return
         for tok_type, token_content, start_line_col, _end_line_col, _line_content in tokens:
             if tokenize.COMMENT == tok_type:
                 line_num = start_line_col[0]
